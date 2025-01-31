4897483b34da773e4bfaeb1bbbbfe5dff0a4d35a
Melike Tetik <98397197+MelikeTetik@users.noreply.github.com>
2025-02-01 02:08:56 +0300

    Update countries.ts

--- geo-quiz/src/data/countries.ts
+++ geo-quiz/src/data/countries.ts
@@ -1,46 +1,230 @@
-// src/countriesAndCapitals.ts
 
-export const countriesAndCapitals = {
-  İspanya: { capital: "Madrid", position: [40.4637, -3.7492] },
-  Fransa: { capital: "Paris", position: [48.8566, 2.3522] },
-  İtalya: { capital: "Roma", position: [41.9028, 12.4964] },
-  Almanya: { capital: "Berlin", position: [52.52, 13.405] },
-  Türkiye: { capital: "Ankara", position: [39.9334, 32.8597] },
-  Namibya: { capital: "Windhoek", position: [-22.5597, 17.0836] },
-  Nauru: { capital: "Yaren", position: [-0.5477, 166.9205] },
-  Nepal: { capital: "Katmandu", position: [27.7172, 85.3240] },
-  Hollanda: { capital: "Amsterdam", position: [52.3676, 4.9041] },
+export const countriesData = {
+  İspanya: { 
+    capital: "Madrid", 
+    position: [40.4637, -3.7492], 
+    flag: "https://flagcdn.com/w320/es.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Fransa: { 
+    capital: "Paris", 
+    position: [48.8566, 2.3522], 
+    flag: "https://flagcdn.com/w320/fr.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  İtalya: { 
+    capital: "Roma", 
+    position: [41.9028, 12.4964], 
+    flag: "https://flagcdn.com/w320/it.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Almanya: { 
+    capital: "Berlin", 
+    position: [52.52, 13.405], 
+    flag: "https://flagcdn.com/w320/de.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Türkiye: { 
+    capital: "Ankara", 
+    position: [39.9334, 32.8597], 
+    flag: "https://flagcdn.com/w320/tr.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Namibya: { 
+    capital: "Windhoek", 
+    position: [-22.5597, 17.0836], 
+    flag: "https://flagcdn.com/w320/na.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Nauru: { 
+    capital: "Yaren", 
+    position: [-0.5477, 166.9205], 
+    flag: "https://flagcdn.com/w320/nr.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Nepal: { 
+    capital: "Katmandu", 
+    position: [27.7172, 85.3240], 
+    flag: "https://flagcdn.com/w320/np.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Hollanda: { 
+    capital: "Amsterdam", 
+    position: [52.3676, 4.9041], 
+    flag: "https://flagcdn.com/w320/nl.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Nikaragua: { 
+    capital: "Managua", 
+    position: [12.1364, -86.2517], 
+    flag: "https://flagcdn.com/w320/ni.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Nijerya: { 
+    capital: "Abuja", 
+    position: [9.0575, 7.4951], 
+    flag: "https://flagcdn.com/w320/ng.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Niue: { 
+    capital: "Alofi", 
+    position: [-19.0544, -169.9187], 
+    flag: "https://flagcdn.com/w320/nu.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Norveç: { 
+    capital: "Oslo", 
+    position: [59.9139, 10.7522], 
+    flag: "https://flagcdn.com/w320/no.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Samoa: { 
+    capital: "Apia", 
+    position: [-13.8333, -172.0833], 
+    flag: "https://flagcdn.com/w320/ws.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Senegal: { 
+    capital: "Dakar", 
+    position: [14.6928, -17.4467], 
+    flag: "https://flagcdn.com/w320/sn.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Sırbistan: { 
+    capital: "Belgrad", 
+    position: [44.8176, 20.4633], 
+    flag: "https://flagcdn.com/w320/rs.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Seyşeller: { 
+    capital: "Victoria", 
+    position: [-4.6167, 55.4513], 
+    flag: "https://flagcdn.com/w320/sc.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Singapur: { 
+    capital: "Singapur", 
+    position: [1.3521, 103.8198], 
+    flag: "https://flagcdn.com/w320/sg.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Somali: { 
+    capital: "Mogadişo", 
+    position: [2.0469, 45.3182], 
+    flag: "https://flagcdn.com/w320/so.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Sudan: { 
+    capital: "Hartum", 
+    position: [15.5000, 32.5592], 
+    flag: "https://flagcdn.com/w320/sd.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Surinam: { 
+    capital: "Paramaribo", 
+    position: [5.866, -55.1712], 
+    flag: "https://flagcdn.com/w320/sr.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Suriye: { 
+    capital: "Şam", 
+    position: [33.5138, 36.2765], 
+    flag: "https://flagcdn.com/w320/sy.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Svaziland: { 
+    capital: "Mbabane", 
+    position: [-26.3042, 31.1343], 
+    flag: "https://flagcdn.com/w320/sz.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Tayland: { 
+    capital: "Bangkok", 
+    position: [13.7563, 100.5018], 
+    flag: "https://flagcdn.com/w320/th.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Togo: { 
+    capital: "Lomé", 
+    position: [6.1375, 1.2123], 
+    flag: "https://flagcdn.com/w320/tg.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Tonga: { 
+    capital: "Nuku'alofa", 
+    position: [-21.139, -175.2047], 
+    flag: "https://flagcdn.com/w320/to.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Tunus: { 
+    capital: "Tunus", 
+    position: [36.8065, 10.1815], 
+    flag: "https://flagcdn.com/w320/tn.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Arjantin: { 
+    capital: "Buenos Aires", 
+    position: [-34.6037, -58.3816], 
+    flag: "https://flagcdn.com/w320/ar.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Brezilya: { 
+    capital: "Brasília", 
+    position: [-15.7801, -47.9292], 
+    flag: "https://flagcdn.com/w320/br.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Kanada: { 
+    capital: "Ottawa", 
+    position: [45.4215, -75.6992], 
+    flag: "https://flagcdn.com/w320/ca.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Çin: { 
+    capital: "Pekin", 
+    position: [39.9042, 116.4074], 
+    flag: "https://flagcdn.com/w320/cn.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Meksika: { 
+    capital: "Mexico City", 
+    position: [19.4326, -99.1332], 
+    flag: "https://flagcdn.com/w320/mx.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Japonya: { 
+    capital: "Tokyo", 
+    position: [35.6762, 139.6503], 
+    flag: "https://flagcdn.com/w320/jp.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Hindistan: { 
+    capital: "Yeni Delhi", 
+    position: [28.6139, 77.2090], 
+    flag: "https://flagcdn.com/w320/in.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
   
-  Nikaragua: { capital: "Managua", position: [12.1364, -86.2517] },
-  Nijerya: { capital: "Abuja", position: [9.0575, 7.4951] },
-  Niue: { capital: "Alofi", position: [-19.0544, -169.9187] },
+  Endonezya: { 
+    capital: "Cakarta", 
+    position: [-6.2088, 106.8456], 
+    flag: "https://flagcdn.com/w320/id.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  İsveç: { 
+    capital: "Stockholm", 
+    position: [59.3293, 18.0686], 
+    flag: "https://flagcdn.com/w320/se.png",
+    questionTypes: ["capital", "country", "flag"]
+  },
+  Polonya: { 
+    capital: "Varşova", 
+    position: [52.2298, 21.0118], 
+    flag: "https://flagcdn.com/w320/pl.png",
+    questionTypes: ["capital", "country", "flag"]
+  }
 
-
-  Norveç: { capital: "Oslo", position: [59.9139, 10.7522] },
- 
- 
-  Samoa: { capital: "Apia", position: [-13.8333, -172.0833] },
-  
-  Senegal: { capital: "Dakar", position: [14.6928, -17.4467] },
-  Sırbistan: { capital: "Belgrad", position: [44.8176, 20.4633] },
-  Seyşeller: { capital: "Victoria", position: [-4.6167, 55.4513] },
- 
-  Singapur: { capital: "Singapur", position: [1.3521, 103.8198] },
-  
-
-  Somali: { capital: "Mogadişo", position: [2.0469, 45.3182] },
-  
-  Sudan: { capital: "Hartum", position: [15.5000, 32.5592] },
-  Surinam: { capital: "Paramaribo", position: [5.866, -55.1712] },
-  Suriye: { capital: "Şam", position: [33.5138, 36.2765] },
-  Svaziland: { capital: "Mbabane", position: [-26.3042, 31.1343] },
-
-  Tayland: { capital: "Bangkok", position: [13.7563, 100.5018] },
- 
-  Togo: { capital: "Lomé", position: [6.1375, 1.2123] },
-  Tonga: { capital: "Nuku'alofa", position: [-21.139, -175.2047] },
-
-  Tunus: { capital: "Tunus", position: [36.8065, 10.1815] },
 };
 
-export default countriesAndCapitals;
+export default countriesData;
+
